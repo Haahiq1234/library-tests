a278daf4ea18d128cf60e4ed9bd286a8a1ea3fe7
Haahiq1234 <100479413+Haahiq1234@users.noreply.github.com>
2024-01-27 18:48:27 +0500

    Tried to draw a circle from scratch

--- Spring/script.js
+++ Spring/script.js
@@ -8,7 +8,7 @@ const SEGMENT_LENGTH = ROPE_LENGTH / ROPE_SEGMENTS;
 const DERP_LENGTH = 25;
 
 const physics = new Physics2DSystem();
-physics.gravity = new Vector2(0, 0.2);
+physics.gravity = new Vector2(0, 0.4);
 physics.SetTimeDependency(false);
 
 let start;
@@ -22,6 +22,8 @@ function setUp() {
     }
     physics.points[0].lock();
     physics.points[physics.points.length - 1].lock();
+    console.log(physics.points[physics.points.length - 1].locked);
+
     for (var i = 0; i < physics.points.length - 1; i++) {
         physics.addSpring(new Physics2DSpring(physics.points[i], physics.points[i + 1], 0.5, 0.15, SEGMENT_LENGTH + 5));
     }
@@ -31,11 +33,13 @@ function setUp() {
 function draw() {
     clear();
     if (mousePressed) {
-        console.log("doing");
+        //console.log("doing");
         let tail = physics.points[physics.points.length - 1];
         tail.pos = Vector.derp(tail.pos, mouse, DERP_LENGTH);
-        tail.vel.set(0, 0);
+        //tail.vel.set(0, 0);
     }
+    console.log(physics.points[physics.points.length - 1].locked);
+
     physics.update();
     for (var point of physics.points) {
         //circle(point.pos.x, point.pos.y, 2);
